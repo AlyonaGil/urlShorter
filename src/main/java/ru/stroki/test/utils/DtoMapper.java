ad7c8872cf2274555a9c3013901a0752fe2a786b
Alyona <Alyona-gil@mail.ru>
2022-10-18 15:47:48 +0500

    add domain

--- src/main/java/ru/stroki/test/utils/DtoMapper.java
+++ src/main/java/ru/stroki/test/utils/DtoMapper.java
@@ -1,14 +1,17 @@
 package ru.stroki.test.utils;
 
-import lombok.experimental.UtilityClass;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import ru.stroki.test.dto.UrlDto;
 import ru.stroki.test.dto.UrlInfoDto;
 import ru.stroki.test.dto.UserDto;
 import ru.stroki.test.entity.Url;
 import ru.stroki.test.entity.User;
-
-@UtilityClass
+@Component
 public final class DtoMapper {
+    @Value("${domain}")
+    private String appDomain;
+
     public UserDto getDto(User user) {
         return UserDto.builder()
                 .id(user.getId())
@@ -18,10 +21,11 @@ public final class DtoMapper {
     }
 
     public UrlDto getDto(Url url) {
+        //todo домен взять из application.properties
         return UrlDto.builder()
                 .id(url.getId())
                 .longUrl(url.getLongUrl())
-                .shortUrl(url.getShortUrl())
+                .shortUrl(appDomain + url.getShortUrl())
                 .createDate(url.getCreateDate())
                 .build();
     }
@@ -29,8 +33,8 @@ public final class DtoMapper {
     public UrlInfoDto getInfoDto(Url url) {
         return UrlInfoDto.builder()
                 .longUrl(url.getLongUrl())
-                .shortUrl(url.getShortUrl())
-                .user(DtoMapper.getDto(url.getUser()))
+                .shortUrl(appDomain + url.getShortUrl())
+                .user(getDto(url.getUser()))
                 .createDate(url.getCreateDate())
                 .countOfViews(url.getTransitions().size())
                 .deleteDate(url.getDeleteDate())
