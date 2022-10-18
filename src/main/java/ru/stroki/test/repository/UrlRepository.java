b41315867a04dfae50871c1311f316b5d2d6172d
Alyona <Alyona-gil@mail.ru>
2022-10-18 15:49:35 +0500

    add methods

--- src/main/java/ru/stroki/test/repository/UrlRepository.java
+++ src/main/java/ru/stroki/test/repository/UrlRepository.java
@@ -1,13 +1,24 @@
 package ru.stroki.test.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.stroki.test.entity.Url;
+import ru.stroki.test.entity.User;
 
-import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UrlRepository extends JpaRepository<Url, Integer> {
-    List<Url> getUrlsByDeleteDateIsNotNull();
+    List<Url> getUrlsByDeleteDateIsNull();
+
+    Optional<Url> findByIdAndUser(Integer id, User user);
+
+    Optional<Url> findByShortUrlAndUser(String shortUrl, User user);
+
+    List<Url> getUrlsByUser(User user);
+
+    @Query(value = "SELECT nextval('url_sequence')", nativeQuery = true)
+    Integer getIntFromSeq();
 }
